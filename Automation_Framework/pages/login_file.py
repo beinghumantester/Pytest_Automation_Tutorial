b176b3089ccdb84a61060bad9ceda100a812a61f
beinghumantester <automatealchemist@gmail.com>
2026-07-22 22:17:31 +0530

    Pages & Conftest.py code updated.

---
+++ Automation_Framework/pages/login_file.py
@@ -0,0 +1,14 @@
+from selenium.webdriver.common.by import By
+import time
+
+
+class LoginPage:
+
+    def login(driver):
+
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+        time.sleep(5)
+        driver.find_element(By.NAME, "username").send_keys("Admin")
+        driver.find_element(By.NAME, "password").send_keys("admin123")
+        driver.find_element(By.XPATH, "//button[@type='submit']").click()
+        time.sleep(5)
--- Automation_Framework/tests/test_login.py
+++ Automation_Framework/tests/test_login.py
@@ -1,13 +1,10 @@
-from selenium.webdriver.common.by import By
-import time
+from pages.login_file import LoginPage
 
 
 def test_login(driver):
+    LoginPage.login(driver)
+ 
 
-    driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-    time.sleep(5)
-    driver.find_element(By.NAME, "username").send_keys("Admin")
-    driver.find_element(By.NAME, "password").send_keys("admin123")
-    driver.find_element(By.XPATH, "//button[@type='submit']").click()
 
-    time.sleep(5)
+
+ 
